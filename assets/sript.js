9a7ec7a4962dfb85f3ded7ecb8d6733b83488ce0
kayla <you@example.com>
2023-07-04 18:35:13 -0700

    added basic CSS and began working on JS

--- assets/sript.js
+++ assets/sript.js
@@ -2,3 +2,7 @@
 // use event listeners to create actions
 
 // create variable called something to do with answer and give it the data of an event listener that logs the users click on one of the possible answers??
+
+var timerCount = document.getElementById('timeLeft');
+
+document.getElementById('advButton').addEventListener('click',)
