fdaf348b2bf49a8db04f6af1c5b71522561f8210
kayla <you@example.com>
2023-07-04 20:30:22 -0700

    began adding variables, functions and event listener.

--- assets/sript.js
+++ assets/sript.js
@@ -3,6 +3,31 @@
 
 // create variable called something to do with answer and give it the data of an event listener that logs the users click on one of the possible answers??
 
-var timerCount = document.getElementById('timeLeft');
 
-document.getElementById('advButton').addEventListener('click',)
+// define variables below
+var startQuiz = document.getElementById('advButton');
+var timeLeft = 300;
+var timerCount = document.getElementById('timeLeft');
+timerCount = "Time Left: " + timeLeft;
+var scoreBoard = document.getElementById('highScoresList');
+var questions = document.getElementById('questions');
+var ansOpt1 = document.getElementById('option1');
+var ansOpt2 = document.getElementById('option2');
+var ansOpt3 = document.getElementById('option3');
+var ansOpt4 = document.getElementById('option4');
+
+
+
+// create functions below
+function beginQuiz() {
+    questions.textContent = 'What breed of cat is typically always female?';
+    ansOpt1.textContent = 'Bengal';
+    ansOpt2.textContent = 'Ragdoll';
+    ansOpt3.textContent = 'Calico';
+    ansOpt4.textContent = 'Sphynx';
+}
+// 
+
+// add event listener to begin quiz/timer
+
+startQuiz.addEventListener('click', beginQuiz);
