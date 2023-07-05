aaee3e66288bafc16f929a1882783a638a4096b0
kayla <you@example.com>
2023-07-04 18:16:49 -0700

    added HTML layout and notes for JS

---
+++ assets/sript.js
@@ -0,0 +1,4 @@
+// use DOM to create varibles for funcitions 
+// use event listeners to create actions
+
+// create variable called something to do with answer and give it the data of an event listener that logs the users click on one of the possible answers??
