2e1c92ddc87cc809204aa4a09f275795cd785c65
kayla <you@example.com>
2023-07-06 21:49:29 -0700

    updated README, fixed issue with timer not stopping at zero and a variety of bug

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# timed-cat-quiz
+## Timed Cat Quiz
+
+
+## Description
+
+As a coding student I wanted to be able to practice using a timed quiz application that emulates an interview quiz. This quiz is meant to test your knowledge of random cat facts in 300 seconds.
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Usage
+
+To use this quiz, first click the Begin button, then select from the answers presented on the screen based on the question given. If an answer selected is incorrect, 100 seconds will be deducted from the timer. Once the timer reaches zero or all questions have been answered, the quiz will end and the user will be presented with an opportunity to record their score on the high scores list and try again. 
+
+## Screenshot
+
+Example of the main page:
+
+![alt text](./assets/Images/main-page.png)
+
+Example of the question display:
+
+![alt text](./assets/Images/question-display.png)
+
+Example of time deduction (top right corner):
+
+![alt text](./assets/Images/time-deduction.png)
+
+Example of the prompt to enter score and initals:
+
+![alt text](./assets/Images/enter-score.png)
+
+Example of the high scores list (top left corner):
+
+![alt text](./assets/Images/display-high-scores.png)
+
+## Link
+
--- assets/sript.js
+++ assets/sript.js
@@ -1,10 +1,4 @@
-// use DOM to create varibles for funcitions 
-// use event listeners to create actions
-
-// create variable called something to do with answer and give it the data of an event listener that logs the users click on one of the possible answers??
-
-
-// define variables below
+// define question and answers object
 var questionsArray = [{
     question: 'What breed of cat is typically always female?',
     answerArray: ['Bengal','Ragdoll','Calico','Sphynx'],
@@ -13,116 +7,151 @@ var questionsArray = [{
 {
     question: 'How many whiskers do cats usually have on each side of their face?',
     answerArray: ['6', '12', '15','32'],
-    correstAns: '12',
+    correctAns: '12',
 },
 {
     question: 'What is a group of cats called?',
     answerArray: ['Clowder', "Meowdur", "Prowl", "Bagel"],
-    correstAns: "Clowder",
+    correctAns: "Clowder",
 }]
 
+//define global variable and HTML selectors
 var startQuiz = document.getElementById('advButton');
 var timeLeft = 300;
 var timeDeduction = 100;
 var timerCount = document.getElementById('timeLeft');
 timerCount.textContent = "Time Left: " + timeLeft;
 var scoreBoard = document.getElementById('highScoresList');
+var hallOfFame = document.getElementById('hall-of-fame');
+var quizContent = document.getElementById('quiz-content');
 var questionText = document.getElementById('questions');
 var ansOpt1 = document.getElementById('option1');
 var ansOpt2 = document.getElementById('option2');
 var ansOpt3 = document.getElementById('option3');
 var ansOpt4 = document.getElementById('option4');
+var inputTextBox = document.getElementById('inputNameText')
 var saveButton = document.getElementById('save-button');
-var currentQuestion = 0
-var score = 0
-
-
+var saveForm = document.getElementById('save-score');
+var finalTime = document.getElementById('time-remaining');
+var finalScore = document.getElementById('score');
+var currentQuestion = 0;
+var score = 0;
+var storeName = document.getElementById('save-name');
+var saveScore = document.getElementById('score');
+var timerInterval;
 
 // create functions below
-// activate quiz questions and timer
 function beginQuiz() {
     startQuiz.style.display = 'none';
+    quizContent.style.display = 'block';
+    //start timer
     countdown();
+    // activate quiz questions
     questionDisplay();
 }
+
 // create timer countdown function and set up interval
 function countdown(){
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         //add interval for each second subtract time by one second and update timer count text content
         timeLeft--;
-        timerCount.textContent = "Time Left: " + timeLeft;
-        //if statement for if timer reaches zero, if so end quiz endQuiz()
-        if (timeLeft === 0) {
-            clearInterval(timerInterval);
-            timerCount.textContent = "oops! all gone :("
-            endQuiz();
-        }
+        timerCount.textContent = "Time Left: " + timeLeft;        
+    //if statement for if timer reaches zero, if so end quiz, if questions are done, end timer
+    if (timeLeft <= 0 || currentQuestion === questionsArray.length) {
+        clearInterval(timerInterval);
+        timerCount.textContent = "oops! all gone :("
+        endQuiz();
+    }
     }, 1000);
-
-
 }
+
 // create question display
 function questionDisplay() {
     // update the content of questions and answers
      //overrite text for questions and answer elements
         questionText.textContent = questionsArray[currentQuestion].question;
         ansOpt1.textContent = questionsArray[currentQuestion].answerArray[0];
+        ansOpt1.setAttribute('value', questionsArray[currentQuestion].answerArray[0]);
+        ansOpt1.onclick = checkAnswer
         ansOpt2.textContent = questionsArray[currentQuestion].answerArray[1];
+        ansOpt2.setAttribute('value', questionsArray[currentQuestion].answerArray[1]);
+        ansOpt2.onclick = checkAnswer
         ansOpt3.textContent = questionsArray[currentQuestion].answerArray[2];
+        ansOpt3.setAttribute('value', questionsArray[currentQuestion].answerArray[2]);
+        ansOpt3.onclick = checkAnswer
         ansOpt4.textContent = questionsArray[currentQuestion].answerArray[3];
+        ansOpt4.setAttribute('value', questionsArray[currentQuestion].answerArray[3]);
+        ansOpt4.onclick = checkAnswer;
 }
 
 // triggered by a guess
-function checkAnswer(event) {
-    //pull text content, compare to correct answer
-    currentQuestion++
-    if (event.target.textContent === questionsArray[currentQuestion].correstAns) {
+//pull text content, compare to correct answer
+function checkAnswer() {
+    console.log(this.value);
+    // tell user if they got it right or wrong and proceed to next question
+    if (this.value === questionsArray[currentQuestion].correctAns) {
+        console.log('correct');
         score++
-        questionDisplay();
     }else {
-        //if wrong subtract time
-        timeLeft -= timeDeduction;
+        console.log('incorrect');
+            //if wrong subtract time
+            timeLeft -= timeDeduction;
+    }
+    currentQuestion++
+    
+    if (currentQuestion === questionsArray.length) {
+        endQuiz();
+    }else {
         questionDisplay();
     }
-    // tell user if they got it right or wrong and proceed to next question
-    
 }
 
 //add end quiz function
 function endQuiz() {
-//clear the questions and present score
-//clear interval needed
-//enterName()
+    //clear the questions and present score
+    quizContent.style.display = 'none';
+    //clear interval needed
+    clearInterval(timerInterval);
+    saveForm.removeAttribute('class', 'hidden'); 
+    //trigger form for entering name
+    enterName()
 }
 
 //create save name after game over
 function enterName() {
-//present form from HTML
-//look up display none/display hidden for CSS
+    //present form from HTML
+    saveForm.setAttribute('data-state', 'visible');
+    //display final score and allow for name input 
+    finalScore.textContent = 'Score: ' + score;
+    inputTextBox.textContent = 'Input initials below: ';
 }
 
 //display scoreboard
 function saveName() {
-//fetch value of input from enterName 
-//add score to highScoresList, store under key
-    //read saved scores, create variable and set equal to local storage
+//fetch value of input from enterName
+    var scoreArray = JSON.parse(localStorage.getItem('Final Scores')) || [];
     //add new score into array
-    //include localstorage getItem, stringify, pars. check notes from class
-    //save new update array/list of scores and overwrite current list
-// display content from highScoresList
+    var newScore = {
+        userScore: score,
+        userName: storeName.value,
+    }
+    scoreArray.push(newScore);
+    localStorage.setItem('Final Scores', JSON.stringify(scoreArray));
 }
 
-// function for start button
-function startButtonDisplay () {
-
+//set function to view high scores
+function highScores() {
+    //read saved scores, create variable and set equal to local storage
+    var scoreArray = JSON.parse(localStorage.getItem('Final Scores')) || [];
+    scoreArray.forEach(element => {
+        var scoreItem = document.createElement('li');
+        scoreItem.textContent = "Name: " + element.userName + " - Score: " + element.userScore;
+        hallOfFame.append(scoreItem);
+    });
 }
-//event listener to hide start button and show questions
+
+scoreBoard.addEventListener("click", highScores);
 //add event listener for submit button 
 saveButton.addEventListener('click', saveName);
 // add event listener to begin quiz/timer
 startQuiz.addEventListener('click', beginQuiz);
-//event listeners for checking the correct answer
-ansOpt1.addEventListener('click', checkAnswer);
-ansOpt2.addEventListener('click', checkAnswer);
-ansOpt3.addEventListener('click', checkAnswer);
-ansOpt4.addEventListener('click', checkAnswer);
